898efc1b43a106d585cc31d441247c5edeb77466
kmehaboobsubhani <105900765+kmehaboobsubhani@users.noreply.github.com>
2022-11-23 23:34:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_subnet" "mehaboob_public_subnet1" {
   availability_zone       = "us-east-1a"
 
   tags = {
-    Name       = "mehaboob_public_subnet1"
+    Name       = "mehaboob_public_subnet1"   
     created_by = "Terraform"
   }
 }
@@ -86,6 +86,61 @@ resource "aws_route_table" "example1" {
 
   tags = {
     Name = "example"
+  }
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {   
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+  vpc_id      = aws_vpc.mehaboob_vpc.id
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group
+  }
+}
+
+
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami             = var.ami_id
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  subnet_id = aws_subnet.mehaboob_public_subnet1.id
+  security_groups = [aws_security_group.my_security_group.id]
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+  tags = {
+    Name = "my_elastic_ip"
   }
 }
 
