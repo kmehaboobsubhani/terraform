426a79fab6c20d731b2ec21ba67aff3b0e319de1
kmehaboobsubhani <105900765+kmehaboobsubhani@users.noreply.github.com>
2022-11-23 23:35:05 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -6,3 +6,33 @@ variable "cidr_block" {
 variable "instance_tenancy" {
   default = "default"
 }
+
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "Jenkins"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-08c40ec9ead489470"
+}
